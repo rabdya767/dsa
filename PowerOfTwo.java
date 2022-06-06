ae360be84a47b6a58516ec13b44f176819e2013d
Sravan <tsravangoud164@gmail.com>
2022-06-06 16:06:01 +0530

    Update

---
+++ PowerOfTwo.java
@@ -0,0 +1,26 @@
+public class PowerOfTwo {
+
+    static int powerOfTwo(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            int power = 2 * powerOfTwo(n - 1);
+            return power;
+        }
+    }
+
+    static int powerOfTwoIT(int n) {
+        int i = 0;
+        int power = 1;
+        while (i < n) {
+            power = power * 2;
+            i = i + 1;
+        }
+        return power;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(powerOfTwo(5));
+        System.out.println(powerOfTwoIT(5));
+    }
+}
---
+++ RecursionLogic.java
@@ -0,0 +1,15 @@
+public class RecursionLogic {
+
+    static void recursiveMethod(int n) {
+        if (n < 1) {
+            System.out.println("n is less than 1");
+        } else {
+            recursiveMethod(n - 1);
+            System.out.println(n);
+        }
+    }
+
+    public static void main(String[] args) {
+        recursiveMethod(4);
+    }
+}
