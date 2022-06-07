4f911ebebf9927c430d6ce044bcc43caa7b71a27
Sravan <tsravangoud164@gmail.com>
2022-06-07 09:34:03 +0530

    Update

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class Fibonacci {
+
+    static int fibonacci(int n) {
+        if (n < 0) {
+            return -1;
+        } else if (n == 0 || n == 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(4));
+    }
+}
